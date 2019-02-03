cea825cff751c6adbb70b53913cf90f9269daf07
Teerapong Dinon <tp.dinon@gmail.com>
2019-02-03 21:18:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ var recipes = {
   flour: '4 cups'
 }
 
-function updateObjectWithKeyAndValue(object, key, value){
-  object[key]= value
-  return object
+function updateObjectWithKeyAndValue(object, key, value) {
+    return Object.assign({}, object, { [key]: value });
 }
